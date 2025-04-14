8b7e0566f3852e3b5df662f400f9520a7881a638
Mohamed768830 <mz2139006@gmail.com>
2025-04-15 01:13:57 +0200

    push

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,6 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     String? token = HelperMethods.getToken();
